4e7882ed314c939913991919e1a83012f0d45c66
Anas <anas.ash099@gmail.com>
2024-10-04 11:32:16 +0200

    remove findByUsername

--- src/main/java/com/passgenius/serviceuser/repository/UserRepository.java
+++ src/main/java/com/passgenius/serviceuser/repository/UserRepository.java
@@ -6,6 +6,6 @@ import com.passgenius.user.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
-    User findByUsername(String username);
+//    User findByUsername(String username);
 
 }
