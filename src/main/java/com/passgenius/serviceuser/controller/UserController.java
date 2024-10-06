d650072b239f08a3e870b7d37586cf64e331317a
Anas <anas.ash099@gmail.com>
2024-10-06 17:51:31 +0200

    remove profile endpoint

--- src/main/java/com/passgenius/serviceuser/controller/UserController.java
+++ src/main/java/com/passgenius/serviceuser/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/profile")
 public class UserController {
     Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
